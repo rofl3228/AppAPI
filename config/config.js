13a9af21c757ae272038fcb1db75d2e2555ec255
rofl3228 <kir.mbr@gmail.com>
2019-01-27 23:19:12 +0300

    Storage config moved to config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,29 @@
 //incert here your Mongo cfg
+const multer = require('multer');
+
 module.exports.db = {
     url : 'mongodb://deteck:walden1998@ds163164.mlab.com:63164/appdb'
 };
 
 //Write port to server app
 module.exports.port = 8000;
+
+//To store directory for avatars
+module.exports.AvatarStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/avatars');
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'ava-'+req.body.user+'-'+file.originalname);
+    }
+});
+
+//To store some images
+module.exports.ImagesStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/img');
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'img-'+req.body.user+'-'+file.originalname);
+    }
+});
